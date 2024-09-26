b9428057bccdb7fb0e3e1f5d4a49e1366cbec7bf
IsoqulovBekzod <isoqulovbekzod6@gmail.com>
2024-09-26 18:48:29 +0500

    ntra

--- example-app/app/Http/Controllers/AdController.php
+++ example-app/app/Http/Controllers/AdController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Ad;
 use App\Models\Branch;
+use App\Models\Status;
 use Faker\Factory;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -14,12 +15,12 @@ class AdController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    public function index()
     {
 
-        $ads= Ad::all();
-        $branches=Branch::all();
-        return view('home' ,compact('ads','branches'));
+            $ads= Ad::all();
+            $branches=Branch::all();
+            return view('home' ,compact('ads','branches'));
 
 
     }
@@ -29,7 +30,7 @@ class AdController extends Controller
      */
     public function create()
     {
-        //
+        return view('components.create-ad');
     }
 
     /**
@@ -43,10 +44,9 @@ class AdController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    public function show(string $id): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory
     {
-        $ads= Ad::find($id);
-        return view('components.single-ad',compact('ads'));
+        return view('components.single-ad', ['ad'=>Ad::query()->find($id)]);
     }
 
     /**
@@ -72,12 +72,17 @@ class AdController extends Controller
     {
         //
     }
-    public function find(Illuminate\Http\Client\Request $request): View|Factory|Application {
+    public function find(Request $request): View|Factory|Application
+    {
+        $ads = \App\Models\Branch::query()->find($request->branch_id)->ads;
 
-        $ads=\App\Models\Branch::query()->find($request->branch_id)->ads();
-
-        $branches=\App\Models\Branch::all();
-        return view('home', compact('ads','branches'));
-
+        $branches = \App\Models\Branch::all();
+        return view('home', compact('ads', 'branches'));
     }
+    public function contact()
+    {
+        return view('components.contact');
+    }
+
+
 }
